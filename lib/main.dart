86a151c5bd0cee9be74e52efd8e7d3c4ce6f8f80
Perez Paredes, Miguel J <mjp4@hw.ac.uk>
2023-02-06 23:26:41 +0000

    - Markers added to a fixed location with the floating button - Changing between screens with the BottomNavigationBar - Placeholder screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:rfg_app/homeScreen.dart';
+import 'package:rfg_app/settings.dart';
+import 'package:rfg_app/listScreen.dart';
+
 void main() {
   runApp(const MyApp());
-}// Prueba
-
+} // Prueba
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -50,19 +51,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -72,50 +61,15 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Stack(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          children: <Widget>[
-            FlutterMap(
-              options: MapOptions(
-                center: LatLng(55.9125188597277, -3.32137120930613),
-              ),
-              children: [
-                TileLayer(
-                  urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-                  userAgentPackageName: 'dev.fleaflet.flutter_map.example',
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        // Changes the center of the screen using the BottomNavigationBar buttons
+          child: currentIndex == 0
+              ? const homeScreen()
+              :currentIndex == 1
+              ? const listScreen()
+              : const settingScreen(),
 
+      ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -129,6 +83,12 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
         selectedItemColor: Colors.blue,
         unselectedItemColor: Colors.grey,
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
