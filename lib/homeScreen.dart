0eaa6e0ee4e07940dc3d4133bb33e00148f59c8b
PaulBooogie <123983528+PaulBooogie@users.noreply.github.com>
2023-02-28 12:16:15 +0000

    Pop Up screens for both trap setting and release are now implemented

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:flutter_map_marker_popup/flutter_map_marker_popup.dart';
+//import 'package:flutter_map_marker_popup/flutter_map_marker_popup.dart';
 import 'package:fluster/fluster.dart';
 
 
