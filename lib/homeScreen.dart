9d3e9a76c217bac5a234237623772b0e82bc1605
PaulBooogie <123983528+PaulBooogie@users.noreply.github.com>
2023-02-28 12:01:10 +0000

    Pop Up screens for both trap setting and release are now implemented

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:flutter_map_marker_popup/flutter_map_marker_popup.dart';
+import 'package:fluster/fluster.dart';
+
 
 
 class homeScreen extends StatefulWidget {
@@ -17,13 +20,59 @@ class _ListState extends State<homeScreen> {
   Widget build(BuildContext context) {
     final markers = markerPoints.map((latlng) {
       return Marker(
-        width: 80,
-        height: 80,
-        point: latlng,
-        builder: (ctx) => Icon(Icons.flag_circle_rounded),
+          width: 80,
+          height: 80,
+          point: latlng,
+          builder: (ctx) => IconButton(
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      scrollable: true,
+                      title: Text('Trap Releaase'),
+                      content: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Form(
+                          child: Column(
+                            children: <Widget>[
+                              TextFormField(
+                                decoration: InputDecoration(
+                                  labelText: 'User ID',
+                                  icon: Icon(Icons.account_box),
+                                ),
+                              ),
+                              TextFormField(
+                                decoration: InputDecoration(
+                                  labelText: 'Password',
+                                  icon: Icon(Icons.lock),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      actions: [
+                        ElevatedButton(
+                          child: Text("Submit"),
+                          onPressed: () {
+                            Navigator.pop(context, 'submit');
+                            addMarker(LatLng(55.9125188597277, -3.32137120930613));
+                          },
+
+                        )
+                      ],
+                    );
+                  });
+            },
+            icon: Icon(Icons.flag_circle_rounded),
+            color: Colors.blue,
+
+          )
       );
     }).toList();
-//test1
+
+
     return Scaffold(
       appBar: AppBar(title: Text("Home Screen")),
       body: Center(
@@ -39,7 +88,7 @@ class _ListState extends State<homeScreen> {
                   userAgentPackageName: 'dev.fleaflet.flutter_map.example',
                 ),
                 MarkerLayer(
-                  markers: markers
+                    markers: markers
                 )
               ],
             ),
@@ -48,16 +97,58 @@ class _ListState extends State<homeScreen> {
       ),
 
       floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          addMarker(LatLng(55.9125188597277, -3.32137120930613));
-        }),
+          child: const Icon(Icons.add),
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    scrollable: true,
+                    title: Text('Trap Setup'),
+                    content: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Form(
+                        child: Column(
+                          children: <Widget>[
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: 'User ID',
+                                icon: Icon(Icons.account_box),
+                              ),
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: 'Password',
+                                icon: Icon(Icons.lock),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    actions: [
+                      ElevatedButton(
+                        child: Text("Submit"),
+                        onPressed: () {
+                          Navigator.pop(context, 'submit');
+                          addMarker(LatLng(55.9125188597277, -3.32137120930613));
+                        },
 
+                      )
+                    ],
+                  );
+                });
+          }),
     );
-    }
+
+
+  }
+
   void addMarker(LatLng latlng){
     setState(() {
       markerPoints.add(latlng);
-    });
+    }
+    );
   }
+
 }
